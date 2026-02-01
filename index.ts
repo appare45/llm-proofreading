83c503c2d98b757adc33ed5e8c1f41620a34ca1b
appare45 <kaibatsu35.7m45@gmail.com>
2026-02-01 12:45:02 +0900

    init octokit

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import { Octokit } from "octokit";
+
+const github_token = Bun.env?.GITHUB_TOKEN;
+if (github_token == undefined) {
+  throw new Error("No github token");
+}
+
+const octokit = new Octokit({ auth: github_token });
+
+const {
+  data: { login },
+} = await octokit.rest.users.getAuthenticated();
+console.log("Hello, %s", login);
