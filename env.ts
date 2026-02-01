58ae46b2df162bb531c549f934a87556fa29da4f
appare45 <kaibatsu35.7m45@gmail.com>
2026-02-01 21:26:41 +0900

    ok

--- env.ts
+++ env.ts
@@ -1,7 +1,7 @@
 export const getEnvOrError = (name: string): string => {
-  const env = Bun.env[name];
-  if (env === undefined) {
-    throw new Error(`Env ${name} is not set`);
-  }
-  return env;
-}
+	const env = Bun.env[name];
+	if (env === undefined) {
+		throw new Error(`Env ${name} is not set`);
+	}
+	return env;
+};
